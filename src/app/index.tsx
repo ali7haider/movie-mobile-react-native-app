4362dffb57289f416ee98dc43a90572a20595ec8
Ali Haider <ali.haider@pentaloop.com>
2026-09-17 18:16:55 +0500

    feat: integrate NativeWind for styling and add Tailwind CSS configuration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-500 font-bold">Movies Mod</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
